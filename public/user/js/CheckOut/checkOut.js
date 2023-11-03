e6e8330b8d5e23bce48305e4916c222005796deb
SWATHINKTK <swathinktk10@gmail.com>
2023-11-03 09:50:30 +0530

    Check out page data retrieval is sucessful.some issues in product Detail page buynow button

--- public/user/js/CheckOut/checkOut.js
+++ public/user/js/CheckOut/checkOut.js
@@ -1,106 +1,107 @@
 
-let address = true;
-// document.addEventListener('DOMContentLoaded',()=>{
+// ****** PROCEED TO PAYMENT - PAYMENT OPTION DIV VIEW *****
+const proceedPaymentBtn = document.getElementById('proceed-payment-btn');
+proceedPaymentBtn.addEventListener('click',()=>{
+
+    const paymentOptionDiv = document.getElementById('payment-options');
+    paymentOptionDiv.style.display = 'block';
+
+    const addressDiv = document.getElementById('checkout-addAddressForm');
+    addressDiv.style.display = 'none';
+
+    const editAddressDiv = document.getElementById('checkout-editAddressForm');
+    editAddressDiv.style.display = 'none';
+});
 
 
-    // ***** ADDING NEW ADDRESS FORM VIEW *****
-    const addNewAddress = document.getElementById('checkout-AddNewAddress');
-    addNewAddress.addEventListener('click',() => {
 
-        const addressDiv = document.getElementById('checkout-addAddressForm');
-        if(address){
-            addressDiv.style.display = 'block';
-            address = false;
-        }else{
-            addressDiv.style.display = 'none';
-            address = true;
-        }
-    });
+// ****** QUANTITY UPDATE TIME UPDATE PRODUCT PRICE IN PRICE VIEW RIGHT SECTION ****
+const priceUpdate = document.getElementById('checkout-product-quanitity');
+priceUpdate.addEventListener('change',(event)=>{
 
+    const newProductQuantity = event.target.value;
+    const hiddenInput = document.getElementById('checkout-hidden-data');
+    console.log(hiddenInput)
+    const oldProductQuantity = hiddenInput.value;
+    const singlePrice = hiddenInput.getAttribute('name');
 
-    // ***** EDIT THE EXISTNG ADDRESS IN CHECKOUT PAGE *****
-    const editCheckoutAddress = document.querySelectorAll('a[name="editCheckoutAddress"]');
-    if(editCheckoutAddress){
+    const checkoutPriceUpdate = document.querySelectorAll('span[name="checkout-price"]');
 
-        editCheckoutAddress.forEach((edit)=>{
+    
 
-            edit.addEventListener('click',async(event)=>{
-                event.preventDefault();
+    if(newProductQuantity > 10){
+        Swal.fire({
+            text: 'Sorry ! Only 10 unit(s) allowed in a single order  If you have a higher requirement, please create a new order.',
+            icon: 'warning',
+            showConfirmButton: false, 
+            timer: 3000,
+            customClass: {
+                icon: 'my-custom-icon-class', 
+                content: 'my-custom-content-class', 
+                }, 
+            });
+            event.target.value = 10;
+            let toalPrice = 10 * singlePrice;
 
-                const id = event.target.getAttribute('data-addressId');
-                const url = `/editaddress${id}?url=true`;
+            checkoutPriceUpdate[0].innerHTML = toalPrice;
+            checkoutPriceUpdate[1].innerHTML = newProductQuantity;
+            checkoutPriceUpdate[2].innerHTML = toalPrice;
+            return;
+    }else if(newProductQuantity <= 0 || newProductQuantity == 'e'){
+        event.target.value = 1;
+        let toalPrice = 1 * singlePrice;
 
-                const response = await fetch(url);
-
-                const responseData = await response.text();
-                console.log(responseData)
-
-                const addressDiv = document.getElementById('checkout-editAddressForm');
-                addressDiv.style.display = 'block';
-                addressDiv.innerHTML = responseData
-
-                const middlePosition = Math.ceil(document.body.scrollHeight / 2.6);
-                window.scrollTo({
-                    top: middlePosition,
-                    behavior: 'smooth' // You can use 'auto' for instant scrolling
-                });
-            })
-        })
+            checkoutPriceUpdate[0].innerHTML = toalPrice;
+            checkoutPriceUpdate[1].innerHTML = newProductQuantity;
+            checkoutPriceUpdate[2].innerHTML = toalPrice;
+        return;
     }
 
 
 
+    let toalPrice = newProductQuantity * singlePrice;
+
+    checkoutPriceUpdate[0].innerHTML = toalPrice;
+    checkoutPriceUpdate[1].innerHTML = newProductQuantity;
+    checkoutPriceUpdate[2].innerHTML = toalPrice;
+    
+    
+    
+})
 
 
-    // ****** PROCEED TO PAYMENT - PAYMENT OPTION DIV VIEW *****
-    const proceedPaymentBtn = document.getElementById('proceed-payment-btn');
-    proceedPaymentBtn.addEventListener('click',()=>{
-
-        const paymentOptionDiv = document.getElementById('payment-options');
-        paymentOptionDiv.style.display = 'block';
-    });
 
 
 
-    // ****** QUANTITY UPDATE TIME UPDATE PRODUCT PRICE IN PRICE VIEW RIGHT SECTION ****
-    const priceUpdate = document.getElementById('checkout-product-quanitity');
-    priceUpdate.addEventListener('change',(event)=>{
+// ****** PLACE AN ORDER *****
+const placeOrder = document.getElementById('placeOrder');
+if(placeOrder){
 
-        const newProductQuantity = event.target.value;
-        const hiddenInput = document.getElementById('checkout-hidden-data');
-        console.log(hiddenInput)
-        const oldProductQuantity = hiddenInput.value;
-        const singlePrice = hiddenInput.getAttribute('name');
+    placeOrder.addEventListener('click',(event)=>{
+
+        const productId = event.target.getAttribute('data-product-id');
+
+        const productQuantity = document.getElementById('checkout-product-quanitity');
         
-
-        if(newProductQuantity > 10){
-            Swal.fire({
-                text: 'Sorry ! Only 10 unit(s) allowed in a single order  If you have a higher requirement, please create a new order.',
-                icon: 'warning',
-                showConfirmButton: false, 
-                timer: 3000,
-                customClass: {
-                    icon: 'my-custom-icon-class', 
-                    content: 'my-custom-content-class', 
-                  }, 
-              });
-              event.target.value = 10;
-              return;
-        }else if(newProductQuantity <= 0 || newProductQuantity == 'e'){
-            event.target.value = 1;
-        }
-
-
-        const checkoutPriceUpdate = document.querySelectorAll('span[name="checkout-price"]');
-        console.log(checkoutPriceUpdate)
-
-        let toalPrice = newProductQuantity * singlePrice;
-
-        checkoutPriceUpdate[0].innerHTML = toalPrice;
-        checkoutPriceUpdate[1].innerHTML = newProductQuantity;
-        checkoutPriceUpdate[2].innerHTML = toalPrice;
+        const deliveryAddress = document.querySelectorAll('input[name="CheckedAddress"]');
         
-        
-        
+        let selectedAddress;
+        deliveryAddress.forEach((address)=>{
+            if(address.checked)
+            {
+                selectedAddress = address;
+            }
+        });
+
+        const payment = document.querySelectorAll('input[name="Payment"]');
+
+        let selectedPaymetOption;
+        payment.forEach((paymentOption)=>{
+            if(paymentOption.checked){
+                selectedPaymetOption = paymentOption;
+            }
+        })
+
+        console.log(productId,productQuantity,selectedAddress,selectedPaymetOption)
     })
-// })
+}
--- public/user/js/CheckOut/checkoutAddress.js
+++ public/user/js/CheckOut/checkoutAddress.js
@@ -1,3 +1,21 @@
+let address = true;
+// ***** ADDING NEW ADDRESS FORM VIEW *****
+const addNewAddress = document.getElementById('checkout-AddNewAddress');
+addNewAddress.addEventListener('click',() => {
+
+    const addressDiv = document.getElementById('checkout-addAddressForm');
+    if(address){
+        addressDiv.style.display = 'block';
+        address = false;
+    }else{
+        addressDiv.style.display = 'none';
+        address = true;
+    }
+});
+
+
+
+
 
 // **** NEW ADDRESS ADDING FORM CHECKOUT PAGE****
 document.addEventListener('DOMContentLoaded',()=>{
@@ -85,73 +103,234 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 
-// ********* EDIT ADDRESS DATA AND SUBMIT ********
-const edit = document.getElementById('editAddress') ? true : false;
-if(edit){
 
-document.getElementById('editAddress').addEventListener('submit',async(event)=>{
-    event.preventDefault();
-    
-    const form = document.getElementById('editAddress');
-    const formData = new FormData(form);
+// ***** EDIT THE EXISTNG ADDRESS IN CHECKOUT PAGE *****
+const editCheckoutAddress = document.querySelectorAll('a[name="editCheckoutAddress"]');
+if(editCheckoutAddress){
 
-    // *** FormData Convert To Normal Object ***
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-        data[key] = value;
-    }
+    editCheckoutAddress.forEach((edit)=>{
 
-    if(validateAddress(data)){
+        edit.addEventListener('click',async(event)=>{
+            event.preventDefault();
 
-        const result = document.getElementById('edit-new-address-sucess');
+            const id = event.target.getAttribute('data-addressId');
+            const url = `/editaddress${id}?url=true`;
 
-        const url = `/updateaddress`;
+            const response = await fetch(url);
 
-        const jsonData = JSON.stringify(data);
-       
+            const responseData = await response.text();
+            console.log(responseData)
 
-        const config = {
-            method: 'POST',
-            body:jsonData,
-            headers:{'Content-Type':'application/json'}
-        }
-        
-        const response = await fetch(url,config);
-
-        if(!response.ok){
-            Window.Location.href = '/error500'
-        }
-
-        const message = await response.json();
-
-        if(message.status){
-
-            window.scrollTo(0, 0);
-            result.style.display = 'block';
-            result.classList.add('alert-success');
+            const addressDiv = document.getElementById('checkout-editAddressForm');
+            addressDiv.style.display = 'block';
             
-            result.innerHTML = `&#9989; ${message.data}`;
-            
+            addressDiv.innerHTML = responseData;
 
-        }else{
+            document.getElementById('editCheckoutAddress').addEventListener('submit',async(event)=>{
+                event.preventDefault();
+               
+                submitCheckoutEditAddress();
+            })
 
-            window.scrollTo(0, 0);
-            result.style.display = 'block';
-            result.classList.add('alert-danger');
-            
-            result.innerHTML = `&#10071; ${message.data}`;
-        }
-
-
-        setTimeout(() => {
-            result.style.display = 'none'; 
-            window.location.href = '/addressinformation';
-        }, 2200);
-    }
-})
+            const middlePosition = Math.ceil(document.body.scrollHeight / 2.6);
+            // window.scrollTo({
+            //     top: middlePosition,
+            //     behavior: 'smooth' // You can use 'auto' for instant scrolling
+            // });
 
 
+
+
+
+
+            // *** ADDING NEW ADDRESS IN CHECKOUT PAGE FORM VALIDATION ***
+            function validateCheckoutAddress(fromData){
+
+                const data = fromData;
+
+                const errorElemetns = document.querySelectorAll('span[name="address-validate-message"]');
+                removeErrorElements();
+
+                // *** REGEX FOR VALIDATION ***
+                const nameRegex = /^[a-zA-Z\s]+$/;
+                const numberRegex = /^[789]\d{9}$/;
+                const pincodeRegex = /^\d{6}$/;
+
+
+                let is_valid = true;
+                
+
+                if(data.Name.split() == ''){
+
+                    errorElemetns[0].innerHTML = ' * please fill out this field.';
+                    is_valid = false;
+
+                }else if(!(data.Name.length >= 2 && data.Name.length <= 50)){
+
+                    errorElemetns[0].innerHTML = ' * name must be between 2 and 50 characters. ';
+                    is_valid = false;
+
+                }else if(!nameRegex.test(data.Name)){
+
+                    errorElemetns[0].innerHTML = ' * character only allowed.';
+                    is_valid = false;
+
+                }else if(data.MobileNumber.split() == ''){
+
+                    errorElemetns[1].innerHTML = ' * enter phonenumber.';
+                    is_valid = false;
+
+                }else if(!numberRegex.test(data.MobileNumber)){
+
+                    if(data.MobileNumber.length != 10){
+
+                        errorElemetns[1].innerHTML = ' * number must be 10 digit. ';
+                        is_valid = false;
+
+                    }else{
+
+                        errorElemetns[1].innerHTML = ' * enter proper format.';
+                        is_valid = false;
+
+                    }
+
+                }else if(!pincodeRegex.test(data.Pincode)){
+
+                    if(data.Pincode.length != 6){
+
+                        errorElemetns[2].innerHTML = ' *  must be enter 6 digit pincode. ';
+                        is_valid = false;
+
+                    }else{
+
+                        errorElemetns[2].innerHTML = ' * enter proper pincode format.';
+                        is_valid = false;
+
+                    }
+
+                }else if(data.Locality.split() == ''){
+
+                    errorElemetns[3].innerHTML = ' * please fill out this field.';
+                    is_valid = false;
+
+                }else if(data.Address.split() == ''){
+
+                    errorElemetns[4].innerHTML = ' * please fill out this field.';
+                    is_valid = false;
+
+                }else if(data.City.split() == ''){
+
+                    errorElemetns[5].innerHTML = ' * please fill out this field.';
+                    is_valid = false;
+
+                }else if(data.District.split() == ''){
+
+                    errorElemetns[6].innerHTML = ' * please select your district.';
+                    is_valid = false;
+
+                }else if(data.AlteranteNumber.length > 0){
+
+                    if(!numberRegex.test(data.AlteranteNumber)){
+
+                        if(data.MobileNumber.length != 10){
+                
+                            errorElemetns[8].innerHTML = ' * number must be 10 digit. ';
+                            is_valid = false;
+                
+                        }else{
+                
+                            errorElemetns[8].innerHTML = ' * enter proper format.';
+                            is_valid = false;
+                
+                        }
+                    }
+                }
+                if(!is_valid){
+                    const middlePosition = Math.ceil(document.body.scrollHeight / 2.6);
+                    window.scrollTo({
+                        top: middlePosition,
+                        behavior: 'smooth' // You can use 'auto' for instant scrolling
+                    });
+                }
+                return is_valid;
+
+            }
+
+            async function submitCheckoutEditAddress(){
+                const form = document.getElementById('editCheckoutAddress');
+                const formData = new FormData(form);
+
+                // *** FormData Convert To Normal Object ***
+                const data = {};
+                for (const [key, value] of formData.entries()) {
+                    data[key] = value;
+                }
+
+                if(validateCheckoutAddress(data)){
+
+                    const result = document.getElementById('edit-new-address-sucess');
+
+                    const url = `/updateaddress`;
+
+                    const jsonData = JSON.stringify(data);
+                
+
+                    const config = {
+                        method: 'POST',
+                        body:jsonData,
+                        headers:{'Content-Type':'application/json'}
+                    }
+                    
+                    const response = await fetch(url,config);
+
+                        if(!response.ok){
+                            Window.Location.href = '/error500'
+                        }
+
+                    const message = await response.json();
+
+                        if(message.status){
+
+                            window.scrollTo({
+                                top: middlePosition,
+                                behavior: 'smooth' // You can use 'auto' for instant scrolling
+                            });
+                            
+                            result.style.display = 'block';
+                            result.classList.add('alert-success');
+                            
+                            result.innerHTML = `&#9989; ${message.data}`;
+                            
+
+                        }else{
+
+                            window.scrollTo({
+                                top: middlePosition,
+                                behavior: 'smooth' // You can use 'auto' for instant scrolling
+                            });
+                            result.style.display = 'block';
+                            result.classList.add('alert-danger');
+                            
+                            result.innerHTML = `&#10071; ${message.data}`;
+                        }
+
+
+                    setTimeout(() => {
+                        result.style.display = 'none'; 
+                        window.location.reload();
+                    }, 2200);
+                }
+            }
+
+
+
+
+        })
+    })
 }
+
+
+
 
 // *** ADDING NEW ADDRESS IN CHECKOUT PAGE FORM VALIDATION ***
 function validateCheckoutAddress(fromData){
