c86cd60bad4bbf993e9606f6d2715d82a7ce8814
SWATHINKTK <swathinktk10@gmail.com>
2023-12-12 14:27:17 +0530

    pdf download setup

--- controller/orderControl.js
+++ controller/orderControl.js
@@ -399,8 +399,6 @@ const orderInvoiceDownload = async(req, res, next) => {
         <script>
             window.onload = function () {
 
-                print();
-
                 const element = document.body; // You can specify any HTML element here
 
                 html2pdf(element);
